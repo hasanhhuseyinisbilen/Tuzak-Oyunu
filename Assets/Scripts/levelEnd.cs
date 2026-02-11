5371690e5ae88660eb7288974fd6b2f34f377b38
hasanhhuseyinisbilen <hasanhuseyinisbilen@gmail.com>
2026-02-11 17:00:24 +0300

    Boş dosya atmışım düzeltildi.

--- Assets/Scripts/levelEnd.cs
+++ Assets/Scripts/levelEnd.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
+using UnityEngine.SceneManagement; 
 
-public class levelEnd : MonoBehaviour
+public class LevelEnd : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private bool triggered = false;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (triggered) return;
+        if (!other.CompareTag("Player")) return;
+
+        triggered = true;
+
+        SnowyLevelManager manager = FindObjectOfType<SnowyLevelManager>();
+        if (manager != null)
+        {
+            int currentIdx = LevelSelector.SelectedLevelIndex; 
+            int reached = PlayerPrefs.GetInt("ReachedLevel", 0); 
+            if (currentIdx == reached)
+            {
+                PlayerPrefs.SetInt("ReachedLevel", reached + 1);
+                PlayerPrefs.Save();
+            }
+              SceneManager.LoadScene(1); 
+        }
     }
 }
