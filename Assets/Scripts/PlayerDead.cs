23966a9a987ba8e00989398d2c5f05dbde1eb91a
hasanhhuseyinisbilen <hasanhuseyinisbilen@gmail.com>
2026-02-11 16:32:56 +0300

    Ölme olayı eklendi oyuncu belli bir seviyenin altına düşünce ölecek belli taglere çarpıncada ölecek ve level baştan bbaşlayacak.3 canıımz olacak ve bu 3 can bittikten sonra bizi level seçme sahnesine atacak.

---
+++ Assets/Scripts/PlayerDead.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerDeath : MonoBehaviour
+{
+    [Header("DeadZone Ayarları")]
+    [SerializeField] private float deathY = -10f; 
+    private bool isDead = false; 
+
+    void Update()
+    {
+      
+        if (!isDead && transform.position.y < deathY)
+        {
+            RestartLevel();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (isDead) return; 
+
+        if (
+            collision.gameObject.CompareTag("Trap") ||
+            collision.gameObject.CompareTag("Icicle") ||
+            collision.gameObject.CompareTag("FlyBox") ||
+            collision.gameObject.CompareTag("Diken")
+           )
+        {
+            RestartLevel();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (isDead) return; 
+
+        if (
+            other.CompareTag("Trap") ||
+            other.CompareTag("Icicle") ||
+            other.CompareTag("FlyBox")||
+            other.CompareTag("Diken")
+           )
+        {
+            RestartLevel();
+        }
+    }
+
+    void RestartLevel()
+    {
+        if (isDead) return; 
+
+        isDead = true; 
+
+  
+        if (LivesManager.Instance != null)
+        {
+            LivesManager.Instance.LoseLife();
+        }
+
+        SnowyLevelManager manager = FindObjectOfType<SnowyLevelManager>();
+        if (manager != null)
+        {
+            manager.RestartLevel();
+        }
+
+        transform.position = Vector3.zero;
+
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.linearVelocity = Vector2.zero;
+        }
+
+        Invoke("ResetDeathFlag", 0.5f);
+    }
+
+    void ResetDeathFlag()
+    {
+        isDead = false;
+    }
+}
