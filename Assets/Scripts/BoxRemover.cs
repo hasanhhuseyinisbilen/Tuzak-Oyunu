d043e46b41b54723228ae524e8f69bff6ed09440
hasanhhuseyinisbilen <hasanhuseyinisbilen@gmail.com>
2026-02-17 13:49:32 +0300

    Kutu pasif hale getirildi.

--- Assets/Scripts/BoxRemover.cs
+++ Assets/Scripts/BoxRemover.cs
@@ -4,19 +4,9 @@ public class BoxRemover : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        
         if (collision.gameObject.CompareTag("Player"))
         {
-            if (TryGetComponent<SpriteRenderer>(out SpriteRenderer sr))
-            {
-                Destroy(sr);
-            }
-            if (TryGetComponent<BoxCollider2D>(out BoxCollider2D bc))
-            {
-                Destroy(bc);
-            }
-            
-
+            gameObject.SetActive(false);
         }
     }
 }
